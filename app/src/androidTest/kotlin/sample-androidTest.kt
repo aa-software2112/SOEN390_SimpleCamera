5a8ad9deada1d1984e93c5d44a5fd310ddd65efc
William-Andrew Lussier <william.lussier115@hotmail.com>
2019-01-18 21:55:19 -0500

    [#11] Add sample Unit Tests
    Directory has been added for the test including a sample test.

---
+++ app/src/androidTest/kotlin/sample-androidTest.kt
@@ -0,0 +1,14 @@
+package test.kotlin
+
+import org.junit.Test
+import org.junit.Assert
+
+class KotlinJunitTest {
+
+    @Test
+    fun firstTest() {
+        Assert.assertTrue(1 == 1)
+    }
+}
+
+
---
+++ app/src/test/kotlin/sample-test.kt
@@ -0,0 +1,19 @@
+package test.kotlin
+
+import org.junit.Test
+import org.junit.Assert
+
+class KotlinJunitTest {
+
+    @Test
+    fun firstTest() {
+        Assert.assertTrue(1 == 1)
+    }
+
+    @Test
+    fun secondTest() {
+        Assert.assertTrue(1 == 1)
+    }
+}
+
+
