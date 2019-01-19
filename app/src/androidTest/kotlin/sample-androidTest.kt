7c5bad2c0b2c877e3d81871fb3b1cc7e2d630b8e
Krishna Patel <thekrish360@hotmail.com>
2019-01-19 15:22:45 -0500

    [#11] Added build task to Graddle and build command in Travis

--- app/src/androidTest/kotlin/sample-androidTest.kt
+++ app/src/androidTest/kotlin/sample-androidTest.kt
@@ -3,12 +3,10 @@ package test.kotlin
 import org.junit.Test
 import org.junit.Assert
 
-class KotlinJunitTest {
+class KotlinJunitTest2 {
 
     @Test
     fun firstTest() {
         Assert.assertTrue(1 == 1)
     }
 }
-
-
