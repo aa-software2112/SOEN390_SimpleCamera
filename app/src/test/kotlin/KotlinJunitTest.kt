cd8a45c69ff97d43b4a56e1ccf3e360bf1c917c2
William <william.lussier115@hotmail.com>
2019-01-22 14:00:29 -0500

    Sample unit test #3

--- app/src/test/kotlin/KotlinJunitTest.kt
+++ app/src/test/kotlin/KotlinJunitTest.kt
@@ -1,11 +1,13 @@
 package test.kotlin
 
 import android.graphics.Rect
-
+import com.nhaarman.mockito_kotlin.*
 import com.simplemobiletools.camera.models.FocusArea
 import com.simplemobiletools.camera.models.MySize
 import org.junit.Test
 import org.junit.Assert
+
+
 
 class KotlinJunitTest {
 
@@ -24,5 +26,16 @@ class KotlinJunitTest {
         println("Testing class FocusArea")
         val mFocusArea = FocusArea(Rect(5,5,5,5),9)
         Assert.assertEquals(9, mFocusArea.weight)
+    }
+
+    @Test
+    fun mainActivity_test() {
+        println("Testing Mockito")
+        val mockedList: MutableList<Int> = mock()
+        mockedList.add(1);
+        mockedList.clear();
+
+        verify(mockedList).add(1);
+        verify(mockedList).clear();
     }
 }
