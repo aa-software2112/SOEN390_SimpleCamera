360de5cd7f394c4acae03d58746fa8f6a6d8386b
William <william.lussier115@hotmail.com>
2019-01-22 14:31:43 -0500

    Spacing
    formatting
    
    
    format fix
    
    
    mock test

--- app/src/test/kotlin/KotlinJunitTest.kt
+++ app/src/test/kotlin/KotlinJunitTest.kt
@@ -1,22 +1,20 @@
 package test.kotlin
 
 import android.graphics.Rect
-import com.nhaarman.mockito_kotlin.*
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
 import com.simplemobiletools.camera.models.FocusArea
 import com.simplemobiletools.camera.models.MySize
 import org.junit.Test
 import org.junit.Assert
 
-
-
 class KotlinJunitTest {
 
-
-    //Sample Tests for Travis CI
+    // Sample Tests for Travis CI
     @Test
     fun mySize_modelsTest() {
         println("Testing class MySize")
-        val mMySize = MySize(16,9)
+        val mMySize = MySize(16, 9)
         Assert.assertEquals(9, mMySize.height)
         Assert.assertEquals(16, mMySize.width)
     }
@@ -24,18 +22,18 @@ class KotlinJunitTest {
     @Test
     fun focusArea_modelsTest() {
         println("Testing class FocusArea")
-        val mFocusArea = FocusArea(Rect(5,5,5,5),9)
+        val mFocusArea = FocusArea(Rect(5, 5, 5, 5), 9)
         Assert.assertEquals(9, mFocusArea.weight)
     }
 
     @Test
-    fun mainActivity_test() {
+    fun mock_test() {
         println("Testing Mockito")
         val mockedList: MutableList<Int> = mock()
-        mockedList.add(1);
-        mockedList.clear();
+        mockedList.add(1)
+        mockedList.clear()
 
-        verify(mockedList).add(1);
-        verify(mockedList).clear();
+        verify(mockedList).add(1)
+        verify(mockedList).clear()
     }
 }
