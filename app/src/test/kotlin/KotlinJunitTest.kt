cd312d16863478b5131e5b373ddadf2e325d4150
William <william.lussier115@hotmail.com>
2019-01-22 13:36:04 -0500

    2 Sample Unit Tests

--- app/src/test/kotlin/KotlinJunitTest.kt
+++ app/src/test/kotlin/KotlinJunitTest.kt
@@ -1,17 +1,28 @@
 package test.kotlin
 
+import android.graphics.Rect
+
+import com.simplemobiletools.camera.models.FocusArea
+import com.simplemobiletools.camera.models.MySize
 import org.junit.Test
 import org.junit.Assert
 
 class KotlinJunitTest {
 
+
+    //Sample Tests for Travis CI
     @Test
-    fun firstTest() {
-        Assert.assertTrue(1 == 1)
+    fun mySize_modelsTest() {
+        println("Testing class MySize")
+        val mMySize = MySize(16,9)
+        Assert.assertEquals(9, mMySize.height)
+        Assert.assertEquals(16, mMySize.width)
     }
 
     @Test
-    fun secondTest() {
-        Assert.assertTrue(1 == 1)
+    fun focusArea_modelsTest() {
+        println("Testing class FocusArea")
+        val mFocusArea = FocusArea(Rect(5,5,5,5),9)
+        Assert.assertEquals(9, mFocusArea.weight)
     }
 }
