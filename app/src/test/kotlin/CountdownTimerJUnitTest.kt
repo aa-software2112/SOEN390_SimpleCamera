4d10cb225f16dd45d4f5d3201bdfaea27cc1578b
Anthony Le <anthonyle96@hotmail.com>
2019-02-09 11:23:37 -0500

    [#18] CountdownTimerJUnitTest now inherits KotlinRoboelectic. KotlinRoboelectic class is "open" to allow inheritance.

--- app/src/test/kotlin/CountdownTimerJUnitTest.kt
+++ app/src/test/kotlin/CountdownTimerJUnitTest.kt
@@ -1,73 +1,15 @@
 package test.kotlin
 
-import android.Manifest
-import android.os.Environment
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import com.simplemobiletools.camera.R
-import com.simplemobiletools.camera.activities.MainActivity
 import org.junit.* // ktlint-disable no-wildcard-imports
-import org.junit.contrib.java.lang.system.EnvironmentVariables
 import org.junit.runner.RunWith
-import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
-import org.robolectric.Shadows
-import org.robolectric.shadows.ShadowApplication
-import org.robolectric.shadows.ShadowEnvironment
 
 @RunWith(RobolectricTestRunner::class)
-class CountdownTimerJUnitTest {
-
-    /**
-    ----------------------------------------------------------------------------------------------------
-    NEED TO REMOVE THE BEGINNING PART & beforeTest() AND INHERIT FROM KotlinRoboelectic.kt INSTEAD
-    ----------------------------------------------------------------------------------------------------
-    */
-
-    /** Create a class variable for storing an activity (a layout from the application) */
-    private var mMainActivity: MainActivity? = null /** MainActivity is a class created by the developer */
-
-    /** A shadow application must be made in order to toggle the camera AND write persmission */
-    var application: ShadowApplication? = null
-
-    /** It is CRUCIAL to have this line of code to set the environment to emulated mode - otherwise
-     * the application will assume the environment is as in the real implementation, and fail to run
-     */
-    @get:Rule
-    public final var env = EnvironmentVariables()
-            .set("EMULATED_STORAGE_TARGET", "SOME_VALUE")
-
-    @Before
-    fun beforeTest() {
-
-        /** Set the environment to correspond to the testing environment */
-        ShadowEnvironment.setExternalStorageState(Environment.MEDIA_MOUNTED)
-
-        /** Displays the environment variable necessary for emulated storage (to run
-         * the application using Robolectric w/o crashing
-         */
-        System.out.println(System.getenv("EMULATED_STORAGE_TARGET"))
-
-        /** Displays the current external storage directory */
-        System.out.println(Environment.getExternalStorageDirectory())
-
-        /**  Gets activity controller attached to the MainActivity */
-        var activityController = Robolectric.buildActivity(MainActivity::class.java)
-
-        /** Extracts the activity */
-        mMainActivity = activityController.get()
-
-        /** Creates a shadow of the application so that permissions can be tacked-onto it */
-        application = Shadows.shadowOf(mMainActivity?.getApplication())
-
-        /** Grants the CAMERA and WRITE_EXTERNAL_STORAGE permissions */
-        application?.grantPermissions(Manifest.permission.CAMERA)
-        application?.grantPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-
-        /** Create, resume, and make visible the MainActivity */
-        mMainActivity = activityController.create().resume().visible().get()
-    }
+class CountdownTimerJUnitTest : KotlinRobolectric() {
 
     /**
     ----------------------------------------------------------------------------------------------------
