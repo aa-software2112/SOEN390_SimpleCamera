4a9fb0c91b3519f6b58bb054b5b70bed4ab1b558
William <william.lussier115@hotmail.com>
2019-03-30 17:46:58 -0400

    Unit Test

--- app/src/test/kotlin/StorageUnitTest.kt
+++ app/src/test/kotlin/StorageUnitTest.kt
@@ -1,20 +1,51 @@
 package test.kotlin
 
+import android.Manifest
+import android.os.Environment
+import android.os.StatFs
+import android.widget.TextView
+import com.simplemobiletools.camera.R
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.RelativeLayout
-import android.widget.TextView
-import com.simplemobiletools.camera.helpers.TIMER_SHORT
-import com.simplemobiletools.camera.helpers.TIMER_MEDIUM
-import com.simplemobiletools.camera.helpers.TIMER_LONG
-import com.simplemobiletools.camera.R
+import com.simplemobiletools.camera.extensions.config
+import com.simplemobiletools.camera.helpers.DeviceStorageUtil
+import com.simplemobiletools.commons.extensions.isVisible
+import com.simplemobiletools.commons.extensions.value
 import org.junit.* // ktlint-disable no-wildcard-imports
+import org.robolectric.shadows.ShadowStatFs
+
 
 @RunWith(RobolectricTestRunner::class)
 class StorageUnitTest : KotlinRobolectric() {
-    
 
+    @Test
+    fun testBytesToHuman() {
+        Assert.assertEquals("1.18 Mb", DeviceStorageUtil.bytesToHuman(1233234))
+    }
+
+    @Test
+    fun testZeroSpace() {
+
+        //Toggle the space remaining
+        mMainActivity?.config?.spaceRemainingOn = true
+        Assert.assertEquals(true, mMainActivity?.config?.spaceRemainingOn)
+
+        mMainActivity?.setRecordingState(true)
+        Assert.assertEquals(true, mMainActivity?.findViewById<TextView>(R.id.space_remaining)?.isVisible())
+        Assert.assertEquals("0 byte left", mMainActivity?.findViewById<TextView>(R.id.space_remaining)?.value)
+    }
+
+    @Test
+    fun testFreeSpace() {
+        //Toggle the space remaining
+        mMainActivity?.config?.spaceRemainingOn = true
+        Assert.assertEquals(true, mMainActivity?.config?.spaceRemainingOn)
+
+        ShadowStatFs.registerStats("/tmp", 100, 1230, 1120)
+        val statsFs = StatFs("/tmp")
+
+        mMainActivity?.setRecordingState(true)
+        Assert.assertEquals(true, mMainActivity?.findViewById<TextView>(R.id.space_remaining)?.isVisible())
+        Assert.assertEquals("4.38 Mb left", mMainActivity?.findViewById<TextView>(R.id.space_remaining)?.value)
+    }
 }
