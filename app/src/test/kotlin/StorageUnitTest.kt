62966be651a4922fbaf55d32b53ea61640138abf
William <william.lussier115@hotmail.com>
2019-03-30 16:46:07 -0400

    File added

---
+++ app/src/test/kotlin/StorageUnitTest.kt
@@ -0,0 +1,20 @@
+package test.kotlin
+
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+import com.simplemobiletools.camera.helpers.TIMER_SHORT
+import com.simplemobiletools.camera.helpers.TIMER_MEDIUM
+import com.simplemobiletools.camera.helpers.TIMER_LONG
+import com.simplemobiletools.camera.R
+import org.junit.* // ktlint-disable no-wildcard-imports
+
+@RunWith(RobolectricTestRunner::class)
+class StorageUnitTest : KotlinRobolectric() {
+    
+
+}
