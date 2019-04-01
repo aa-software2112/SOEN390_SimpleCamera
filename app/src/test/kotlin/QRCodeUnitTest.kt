82aa3d16d32a1395424a1d0d7b0c5fd9cdef9bc9
Samuel Gosselin <samuelgosselin1@hotmail.com>
2019-03-31 22:03:46 -0400

    [#102] first test

--- app/src/test/kotlin/QRCodeUnitTest.kt
+++ app/src/test/kotlin/QRCodeUnitTest.kt
@@ -5,8 +5,25 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import kotlin.test.assertNotNull
+
 @RunWith(RobolectricTestRunner::class)
 class QRCodeUnitTest : KotlinRobolectric() {
+
+
+    @Test
+    fun testQRScannerInited() {
+        Assert.assertNotNull(mMainActivity?.mQrScanner)
+    }
+
+    // Once the MainActivity has been launched, the singleton should have had the methods "setContext",
+    // "setApplication", "build" and "setCameraPreview" called (you can do .verify on these).
+    @Test
+    fun testQRmethodsCalled() {
+
+    }
     //Once you know QRScanner.qr_requested is true, wait for it to turn false; you should check that addQrPhoto//(...) was called at this point, along with scanPhotos().
     @Test
     fun testAddQrPhoto_Called() {
